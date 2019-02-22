70da49ebe836b452ebe7b5bd56cf094c07bc00a5
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-21 23:53:28 -0500

    Added pages

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,26 +1,13 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class HomeController {
     @RequestMapping("/") //or @GetMapping("/index")
     public String index(){
         return "index";
-    }
-
-    @RequestMapping("/registration")
-    public String registration(){
-        return "registration";
-    }
-
-    //test
-    @RequestMapping("/index1")
-    public String index1(){
-        return "index1";
     }
 
     @RequestMapping("/movie1")
